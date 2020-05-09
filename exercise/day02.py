136924aa3741483e3955c9d77a73d26b01ad9f36
jc02 <congde_mail@163.com>
2020-05-09 15:55:55 +0800

    rgb图像转换为灰度图像

---
+++ exercise/day02.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+src = cv.imread("../images/beauty.jpg")
+cv.namedWindow("input",cv.WINDOW_AUTOSIZE)
+cv.imshow("input",src)
+
+gray = cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+cv.imshow("gray",gray)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
